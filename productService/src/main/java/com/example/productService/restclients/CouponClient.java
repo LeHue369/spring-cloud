41209e8786a72b03cdf10f5b68af1d0b3a3bc539
Lethihue030699 <lethihue030699@gmail.com>
2023-07-14 14:34:00 +0700

    add Ribbon and Zuul configuration

--- productService/src/main/java/com/example/productService/restclients/CouponClient.java
+++ productService/src/main/java/com/example/productService/restclients/CouponClient.java
@@ -1,13 +1,15 @@
 package com.example.productService.restclients;
 
 import com.example.productService.entity.Coupon;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "gateway-service")
+@FeignClient(name = "zuul-api-gateway")
+//@RibbonClient(name = "coupon-service")
 public interface CouponClient {
 
-	@GetMapping("/coupons/find")
+	@GetMapping("/coupon-service/coupons/find")
 	Coupon getCoupon(@RequestParam("code") String code);
 }
